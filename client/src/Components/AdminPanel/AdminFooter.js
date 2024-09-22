f7d7ccd0db5dcba4cb18d3384577a985f7f41c29
Sankalp Singh <singhsankalp997@gmail.com>
2024-09-22 19:01:52 +0530

    admin panel

---
+++ client/src/Components/AdminPanel/AdminFooter.js
@@ -0,0 +1,9 @@
+import React from 'react';
+function AdminFooter() {
+  return (
+    <footer className="admin-footer">
+      <p>&copy;{new Date().getFullYear()} Admin Panel.All rights reserved</p>
+    </footer>
+  );
+}
+export default AdminFooter;
